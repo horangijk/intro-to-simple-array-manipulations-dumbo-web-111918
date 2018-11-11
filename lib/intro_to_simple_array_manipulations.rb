e28f98519a0e904f440ff36aec9eb0e232a3cdb2
Jerold Kim <iamjooyoung@gmail.com>
2018-11-11 14:34:57 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,8 +7,8 @@ end
 def using_pop(array)
   array.pop 
 end 
-def pop_with_args(array, 2)
-  2.times {array.pop}
+def pop_with_args(array, num)
+  num.times {array.pop}
 end 
 def using_shift(array)
   array.shift 
